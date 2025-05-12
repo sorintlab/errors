32459a7cc7a1c3b275917186bf5ae75ab7b92c94
Simone Gotti <simone.gotti@gmail.com>
2025-05-12 09:48:36 +0200

    format: handle multiple wrapped errors (join error).
    Correctly unwrap join errors (like the one provided by stdlib or the
    equivalent ones) that provides the `Unwrap() []errors` method and print
    them like a tree showin the error number in the provided list.

--- format.go
+++ format.go
@@ -1,44 +1,108 @@
 package errors
 
 import (
-	"errors"
 	"fmt"
+	"strings"
 )
 
-func PrintErrorDetails(err error) []string {
-	type stackTracer interface {
-		StackTrace() StackTrace
+func unwrap(err error) []error {
+	switch x := err.(type) {
+	case interface{ Unwrap() error }:
+		err = x.Unwrap()
+		if err == nil {
+			return nil
+		}
+		return []error{err}
+	case interface{ Unwrap() []error }:
+		errs := []error{}
+		for _, err := range x.Unwrap() {
+			if err == nil {
+				continue
+			}
+			errs = append(errs, err)
+		}
+		return errs
+	default:
+		return nil
 	}
-	type errWithStack struct {
-		err   error
-		msg   string
-		stack StackTrace
+}
+
+type stackTracer interface {
+	StackTrace() StackTrace
+}
+
+type errWithStack struct {
+	err   error
+	msg   string
+	stack StackTrace
+	level int // level is increased when the wrapped errors are a more than 1
+	index int // index is the wrapped errors index (in case of more than one wrapped error)
+}
+
+func getStackErr(errCause error, level int, index int) []errWithStack {
+	var stackErrs []errWithStack
+
+	stackErr := errWithStack{
+		err:   errCause,
+		msg:   errCause.Error(),
+		level: level,
+		index: index,
+	}
+	if s, ok := errCause.(stackTracer); ok {
+		stackErr.stack = s.StackTrace()
+	}
+	stackErrs = append(stackErrs, stackErr)
+	errCauses := unwrap(errCause)
+
+	if len(errCauses) > 1 {
+		level++
 	}
 
-	var stackErrs []errWithStack
-	errCause := err
-	for errCause != nil {
-		stackErr := errWithStack{
-			err: errCause,
-			msg: errCause.Error(),
-		}
-		if s, ok := errCause.(stackTracer); ok {
-			stackErr.stack = s.StackTrace()
-		}
-		stackErrs = append(stackErrs, stackErr)
-		errCause = errors.Unwrap(errCause)
-		if err == nil {
-			break
-		}
+	for i, errCause := range errCauses {
+		stackErrs = append(stackErrs, getStackErr(errCause, level, i)...)
 	}
+
+	return stackErrs
+}
+
+func PrintErrorDetails(err error) []string {
+	stackErrs := getStackErr(err, 0, 0)
 
 	var lines []string
+	curLevel := 0
+	curIndex := 0
 	for _, stackErr := range stackErrs {
+		printIndex := false
+		if curLevel != stackErr.level {
+			printIndex = true
+		}
+		if curIndex != stackErr.index {
+			printIndex = true
+		}
+		curLevel = stackErr.level
+		curIndex = stackErr.index
+
+		indexstr := ""
+		indexstrph := ""
+		if stackErr.level > 0 {
+			indexstr = fmt.Sprintf("[%d] ", stackErr.index)
+			indexstrph = strings.Repeat(" ", len(indexstr))
+		}
+		if !printIndex {
+			indexstr = indexstrph
+		}
+
+		newlines := []string{}
 		if len(stackErr.stack) > 0 {
 			frame := stackErr.stack[0]
-			lines = append(lines, fmt.Sprintf("(%T) %+v: %s", stackErr.err, frame, stackErr.msg))
+			newlines = append(newlines, fmt.Sprintf("%s(%T) %s", indexstr, stackErr.err, frame.name()))
+			newlines = append(newlines, fmt.Sprintf("\t%s:%d: %s", frame.file(), frame.line(), stackErr.msg))
 		} else {
-			lines = append(lines, fmt.Sprintf("(%T) %s", stackErr.err, stackErr.msg))
+			newlines = append(newlines, fmt.Sprintf("%s(%T) %s", indexstr, stackErr.err, stackErr.msg))
+		}
+		for _, newline := range newlines {
+			newline = strings.Repeat("\t", stackErr.level) + newline
+			lines = append(lines, newline)
 		}
 	}
 
